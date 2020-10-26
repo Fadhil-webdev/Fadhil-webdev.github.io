4d4632e53a4471ea3667270fadbcec3a52ea9259
DaengLekir <71427561+Fadhil-webdev@users.noreply.github.com>
2020-10-26 23:17:21 +0800

    recon

--- table-booking-app/scripts/list.js
+++ table-booking-app/scripts/list.js
@@ -1,94 +1,61 @@
-<!doctype html>
-<html lang="en">
+window.addEventListener("load", function () {
+    GetBookings();
+});
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+function GetBookings() {
+    let url = 'https://api.apispreadsheets.com/data/2524/';
+    fetch(url)
+        .then((response) => response.json())
+        .then(json => {
+            // Do something with the data
+            // console.log(json.bookings);
+            var bookings = document.getElementById("booking-list");
+            var bookingIds = [];
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+            for (var i = 0; i < json.bookings.length; i++) {
+                var gName = json.bookings[i].name;
+                var gEmail = json.bookings[i].email;
+                var gPax = json.bookings[i].pax;
+                var gId = json.bookings[i].id;
+                var gRemarks = json.bookings[i].remarks;
+                var buttonId = "delete" + gId;
 
-    <script src="scripts/list.js"></script>
-    <title>Booking Listing - Table Booking App</title>
-</head>
+                let row = bookings.insertRow(bookings.rows.length);
+                row.insertCell(0).innerHTML = gId;
+                row.insertCell(1).innerHTML = gName;
+                row.insertCell(2).innerHTML = gEmail;
+                row.insertCell(3).innerHTML = gPax;
+                row.insertCell(4).innerHTML = gRemarks; //Remarks
+                row.insertCell(5).innerHTML = "<button id='" + buttonId + "' class='btn btn-danger'>Delete</button><br/>";
 
-<body>
-    <div class="container-fluid">
-        <!-- NavBar -->
-        <div class="row">
-            <div class="col">
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <a class="navbar-brand" href="#">
-                        <img src="https://assets.codepen.io/4530873/booking.svg" width="40" />
-                        Table Booking App
-                    </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse"
-                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
+                bookingIds.push(buttonId);
 
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item">
-                                <a class="nav-link" href="index.html">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="book.html">Book Table</a>
-                            </li>
-                            <li class="nav-item active">
-                                <a class="nav-link" href="list.html">View Bookings</a>
-                            </li>
-                        </ul>
+            }
 
-                    </div>
-                </nav>
-            </div>
-        </div>
-
-        <!-- Jumbotron -->
-        <div class="row">
-            <div class="col">
-                <div class="jumbotron">
-                    <h1 class="display-4">Here it is our booking list!</h1>
-                    <p class="lead">Call them to confirm the booking</p>
-
-                </div> <!-- jumbotron element -->
-            </div> <!-- jumbotron col -->
-        </div> <!-- jumbotron row -->
-
-        <!-- Booking List -->
-        <div class="row">
-            <div class="col">
-                <table class="table" id="booking-list">
-                    <thead class="thead-light">
-                        <tr>
-                            <th scope="col">Id</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Pax</th>
-                            <th scope="col">Remarks</th>
-                            <th scope="col">Task</th>
-                        </tr>
-                    </thead>
-                    <tbody></tbody>
-                </table>
-            </div> <!-- Booking List col-->
-        </div> <!-- Booking List row-->
-
-    </div>
-
-    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
-        crossorigin="anonymous"></script>
+            for (let j = 0; j < bookingIds.length; j++) {
+                //console.log(bookingIds[j]);
+                let el = document.getElementById(bookingIds[j]);
+                el.addEventListener("click", function () {
+                    let theId = bookingIds[j].replace("delete", "");
+                    DeleteBooking(theId);
+                });
+            }
 
 
-</body>
+        });
+}
 
-</html>
+function DeleteBooking(id) {
+
+    if (confirm("Are you sure you want to delete?")) {
+        let url = 'https://api.apispreadsheets.com/data/2524/' + id;
+        fetch(url, {
+            method: 'DELETE',
+        })
+            .then((response) => {
+                location.reload();
+            });
+    } else {
+        alert("Delete cancelled");
+    }
+}
