792882c520454efe3fe3d6bd2146185ccd26458e
DaengLekir <71427561+Fadhil-webdev@users.noreply.github.com>
2020-11-04 22:24:40 +0800

    another page

--- table-booking-app/scripts/home.js
+++ table-booking-app/scripts/home.js
@@ -2,11 +2,6 @@
 let header = document.querySelector('.header');
 let hamburgerMenu = document.querySelector('.hamburger-menu');
 
-window.addEventListener('scroll', function(){
-    let windowPosition = window.scrollY > 0;
-    header.classList.toggle('active', scrollY > 0)
-});
-
 hamburgerMenu.addEventListener("click", function(){
     header.classList.toggle('menu-open');
 });
